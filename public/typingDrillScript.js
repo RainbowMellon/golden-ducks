282f38379d1d3291ba8bdfa18f9261a986489986
georgelmcghee <georgelmcghee@gmail.com>
2026-02-11 03:30:25 +0000

    cont

--- public/typingDrillScript.js
+++ public/typingDrillScript.js
@@ -1,5 +1,5 @@
 const inputField = document.querySelector('input[name="drillInput"]');
-const correctInput = document.querySelector('div[name="correctInput"]');
+const correctInput = document.querySelector('p[name="correctInput"]');
 let inputValue = '';
 inputField.addEventListener('input', (event) => {
     inputValue = event.target.value;
@@ -10,7 +10,7 @@ submitButton.addEventListener('click', () => {
     for(const char of inputValue){
         console.log(char);
     }
-    console.log(correctInput);
+    console.log(correctInput.textContent);
 });
 
 
