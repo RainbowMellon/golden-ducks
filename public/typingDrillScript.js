9f30e93b6328246499767f8681c4d33a16658946
georgelmcghee <georgelmcghee@gmail.com>
2026-02-11 02:14:29 +0000

    submit button

--- public/typingDrillScript.js
+++ public/typingDrillScript.js
@@ -2,6 +2,10 @@ const inputField = document.querySelector('input[name="drillInput"]');
 let inputValue = '';
 inputField.addEventListener('input', (event) => {
     inputValue = event.target.value;
+  });
+
+const myButton = document.querySelector('input[name="submit"]');
+myButton.addEventListener('click', () => {
     console.log(inputValue);
-  }
-);
+    alert('Button was clicked!');
+});
