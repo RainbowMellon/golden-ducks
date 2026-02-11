8fe79752b3d92dcce1d15488f708121cb70c31df
georgelmcghee <georgelmcghee@gmail.com>
2026-02-11 03:28:05 +0000

    checking all the chars of the input string

--- public/typingDrillScript.js
+++ public/typingDrillScript.js
@@ -1,10 +1,16 @@
 const inputField = document.querySelector('input[name="drillInput"]');
+const correctInput = document.querySelector('div[name="correctInput"]');
 let inputValue = '';
 inputField.addEventListener('input', (event) => {
     inputValue = event.target.value;
   });
 
-const myButton = document.querySelector('button[name="submit"]');
-myButton.addEventListener('click', () => {
-    console.log(inputValue);
+const submitButton = document.querySelector('button[name="submit"]');
+submitButton.addEventListener('click', () => {
+    for(const char of inputValue){
+        console.log(char);
+    }
+    console.log(correctInput);
 });
+
+
